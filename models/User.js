eeac0b91b66b0caf3865c5dcd4331df1b57fccc2
Vivek Maddeshiya <57106245+vivekiet22@users.noreply.github.com>
2023-03-10 13:37:27 +0530

    added user model for the database

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide a name"]
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide an email"],
+    unique: true,
+    validate: [validator.isEmail, "Please provide a valid email"]
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minlength: 8,
+    select: false
+  },
+  
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
