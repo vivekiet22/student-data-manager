3293b349507c8887c051d7f24990bd36ba3cc38b
Vivek Maddeshiya <57106245+vivekiet22@users.noreply.github.com>
2023-03-22 18:13:04 +0530

    added mongodb atlas to store on server and added path module

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const path = require('path')
 
 const authRoute = require("./routes/authRoute");
-
 const dashboardRoute = require("./routes/dashboardRoute");
 
 let cors = require('cors')
 
 
-
 dotenv.config();
+
 const app = express();
 
 
-
-const DB = 'mongodb://127.0.0.1:27017/student_data_manager'
+const DB = process.env.DB
+// const DB = 'mongodb://127.0.0.1:27017/student_data_manager'
 try {
     mongoose.connect(DB).then(()=>console.log("Connected to DB Successfully"));
   } catch (error) {
@@ -30,6 +30,14 @@ app.use(cors())
 
 app.use("/", authRoute);
 app.use("/dashboard",dashboardRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 
 const port = 5000;
