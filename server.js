e927cd928558f4e5a19be10698f1cd6558d5d8ee
Vivek Maddeshiya <57106245+vivekiet22@users.noreply.github.com>
2023-03-24 10:52:45 +0530

    modified: changed server.js port Variable

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 
-const port = 5000;
+const port =process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Listening from port ${port}`);
